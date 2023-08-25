bdd57e5a1426b17b7e7b576be516e9e802408562
MaxMitin <maksim.mitin@mail.ru>
2023-08-25 16:36:16 +0300

    Corrected

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,112 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Random;
+import java.util.Scanner;
+
+class Main {
+    final char SIGN_X = 'X';
+    final char SIGN_O = 'O';
+    final char SIGN_EMPTY = '.';
+    char[][] table;
+    Random random;
+    Scanner scanner;
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        new Main().game();
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    Main() {
+        random = new Random();
+        scanner = new Scanner(System.in);
+        table = new char[5][5];
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    void game() {
+        initTable();
+        while (true) {
+            turnHuman();
+            if (checkWin(SIGN_X)) {
+                System.out.println("Вы выиграли!");
+                break;
+            }
+            if (isTableFull()) {
+                System.out.println("Хода больше нет!");
+                break;
+            }
+            turnAI();
+            printTable();
+            if (checkWin(SIGN_O)) {
+                System.out.println("Компьютер выиграл!");
+                break;
+            }
+            if (isTableFull()) {
+                System.out.println("Хода больше нет!");
+                break;
+            }
         }
+        System.out.println("Игра завершена!");
+        printTable();
+    }
+
+    void initTable() {
+        for (int row = 0; row < 5; row++)
+            for (int col = 0; col < 5; col++)
+                table[row][col] = SIGN_EMPTY;
+    }
+
+    void printTable() {
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 5; col++)
+                System.out.print(table[row][col] + " ");
+            System.out.println();
+        }
+    }
+
+    void turnHuman() {
+        int x, y;
+        do {
+            System.out.println("Введите два целочесленных значения точки Икс в диапазоне с '1' по '5'!");
+            x = scanner.nextInt() - 1;
+            y = scanner.nextInt() - 1;
+        } while (!isCellValid(x, y));
+        table[y][x] = SIGN_X;
+    }
+
+    boolean isCellValid(int x, int y) {
+        if (x < 0 || y < 0 || x >= 5|| y >= 5)
+            return false;
+        return table[y][x] == SIGN_EMPTY;
+    }
+
+    void turnAI() {
+        int x, y;
+        do {
+            x = random.nextInt(5);
+            y = random.nextInt(5);
+        } while (!isCellValid(x, y));
+        table[y][x] = SIGN_O;
+    }
+
+    boolean checkWin(char dot) {
+        for (int i = 0; i < 5; i++)
+            if ((table[i][0] == dot && table[i][1] == dot &&
+                    table[i][4] == dot) ||
+                    (table[0][i] == dot && table[1][i] == dot &&
+                            table[4][i] == dot))
+                return true;
+        if ((table[0][0] == dot && table[1][1] == dot &&
+                table[4][4] == dot) ||
+                (table[4][0] == dot && table[1][1] == dot &&
+                        table[0][4] == dot))
+            return true;
+        return false;
+    }
+
+    boolean isTableFull() {
+        for (int row = 0; row < 5; row++)
+            for (int col = 0; col < 5; col++)
+                if (table[row][col] == SIGN_EMPTY)
+                    return false;
+        return true;
     }
 }
